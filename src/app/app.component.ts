b1a2b279cd169f9a1e9e34620b55645a4c9a25f7
mites-vasoya <78782844+mites-vasoya@users.noreply.github.com>
2024-08-25 09:31:09 +0530

    google analytics added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AnalyticsService} from "./services/analytics.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'devaid';
+
+  constructor(private analyticsService: AnalyticsService) {
+  }
+
+  ngOnInit() {
+    this.analyticsService.trackEvent("landing_page_view", "Home page Loaded", "page_view")
+  }
 }
